f0ee880dcafb3dfbd55fe06f08d5d4e116c52727
keke18532 <keke18532@gmail.com>
2016-10-03 23:34:51 +0300

    Create master.py

---
+++ master.py
@@ -0,0 +1,16 @@
+import socket
+import linecache
+
+
+host=socket.gethostname()
+
+lines=linecache.getlines('config.txt')
+for i in range(0,len(lines)):
+	s=socket.socket()
+	if(s.connect_ex((host,int(lines[i].strip().split(' ')[1])))==0):
+		print('okay')
+		msg='yes'
+		s.send(bytes(msg, 'utf-8'))
+		print('sent')
+	s.close()
+	
